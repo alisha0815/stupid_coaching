97c85d0f9c3b8d503edde084d46b40082597f275
jungah sohn <aliciasohn815@gmail.com>
2021-07-05 07:32:15 +0200

    rails stupid coaching in progress

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+    @question = params[:question]
+  end
+
+  def answer
+    @search = params[:question]
+
+    if @search.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @search == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  
+  end
+end
